d6ab05a690ce830f8ae0f6dea86692b30e1eb7b7
Samuel Le Berrigaud <samuel.lb@gmail.com>
2010-08-24 15:00:29 +0200

    - Fixing Mysql definition

--- src/main/groovy/org/leberrigaud/maven/plugins/database/MySql.groovy
+++ src/main/groovy/org/leberrigaud/maven/plugins/database/MySql.groovy
@@ -8,9 +8,9 @@ final class MySql
     final String driver = "com.mysql.jdbc.Driver"
     final String url = "jdbc:mysql://localhost:3306/mysql"
 
-    final String createUser = {username, password -> "create user '$username'@'localhost' identified by '$password'" }
-    final String createDb = {name -> "create database $name" }
-    final String grantPrivileges = {dbName, user -> "grant all on ${dbName}.* to '$user'@'localhost';" }
-    final String dropUser = {username -> "drop user '$username'@'localhost'" }
-    final String dropDb = {name -> "drop database $name" };
+    final def createUser = {username, password -> "create user '$username'@'localhost' identified by '$password'" }
+    final def createDb = {name -> "create database $name" }
+    final def grantPrivileges = {dbName, user -> "grant all on ${dbName}.* to '$user'@'localhost';" }
+    final def dropUser = {username -> "drop user '$username'@'localhost'" }
+    final def dropDb = {name -> "drop database $name" };
 }
