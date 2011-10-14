fde540fb30cebf20e9b677ca75feeb7883e2eec4
Samuel Le Berrigaud <sam@leberrigaud.org>
2011-10-14 16:38:57 +0200

    Bah!

--- src/test/groovy/org/leberrigaud/maven/plugins/database/Script.groovy
+++ src/test/groovy/org/leberrigaud/maven/plugins/database/Script.groovy
@@ -2,37 +2,17 @@ package org.leberrigaud.maven.plugins.database
 
 final mojo = new CreateMojo()
 
-//mySql(mojo)
-sqlServer(mojo)
+// db admin
+mojo.database = 'sqlserver'
 
-private def sqlServer(CreateMojo mojo)
-{
-    // db admin
-    mojo.database = 'sqlserver'
+mojo.rootUsername = 'sa'
+mojo.rootPassword = '57ia7ouq'
 
-    mojo.rootUsername = 'sa'
-    mojo.rootPassword = '57ia7ouq'
+// host config
+mojo.host = '192.168.0.106'
+mojo.port = '1433'
 
-    // host config
-    mojo.host = '192.168.0.106'
-    mojo.port = '1433'
-
-    mojo.schema = 'ao_schema'
-}
-
-private def mySql(CreateMojo mojo)
-{
-    // db admin
-    mojo.database = 'mysql'
-
-    mojo.rootUsername = 'root'
-    mojo.rootPassword = ''
-
-    // host config
-    mojo.host = 'localhost'
-    mojo.port = '3306'
-}
-
+mojo.schema = 'ao_schema'
 
 // user and database created
 mojo.name = 'ao_test'
